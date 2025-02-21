0199751b24079e4a6ab5e1ab5aaabb648c6532ba
umbrellazg <2259936975@qq.com>
2025-02-21 16:16:38 +0800

    一个示例的client部分，包含一个hello服务

---
+++ client/src/main/java/socket.java
@@ -0,0 +1,16 @@
+import config.RpcServiceConfig;
+import hello.Hello;
+import hello.HelloService;
+import proxy.RpcClientProxy;
+import remoting.transport.RpcRequestTransport;
+
+public class SocketClientMain {
+    public static void main(String[] args) {
+        RpcRequestTransport rpcRequestTransport = new SocketRpcClient();
+        RpcServiceConfig rpcServiceConfig = new RpcServiceConfig();
+        RpcClientProxy rpcClientProxy = new RpcClientProxy(rpcRequestTransport, rpcServiceConfig);
+        HelloService helloService = rpcClientProxy.getProxy(HelloService.class);
+        String hello = helloService.hello(new Hello("111", "222"));
+        System.out.println(hello);
+    }
+}
